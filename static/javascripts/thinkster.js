342b4df4d6fa208ce1716e589e68b62ea43d7f33
ramcn <cn.ramachandra@gmail.com>
2015-05-10 00:03:16 +0530

    update 1

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -1,2 +1,17 @@
-angular
-  .module('thinkster', []);
+(function () {
+  'use strict';
+
+  angular
+    .module('thinkster', [
+      'thinkster.config',
+      'thinkster.routes',
+      'thinkster.authentication'
+    ]);
+
+  angular
+    .module('thinkster.config', []);
+
+  angular
+    .module('thinkster.routes', ['ngRoute']);
+})();
+
